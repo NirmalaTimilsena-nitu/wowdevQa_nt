46becda1f621c868b6240d091429527c829d390a
Nirmala Timilsena <anitim26@gmail.com>
2021-06-01 15:41:51 +0545

    modified codes

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(executable_path='C:\\chromedriver')
-driver.get('http://www.google.com')
+from time import sleep
+while True:
+    driver = webdriver.Chrome(executable_path='C:\\chromedriver')
+    driver.get('https://www.youtube.com')
+    driver.maximize_window()
+    
+    sleep(3)
+    driver.close()
